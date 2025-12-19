888d473d164fb548067050441c5eca0476503e1e
sindhuja-123-SS <sindhuja.ss2024aiml@sece.ac.in>
2025-12-19 09:50:51 +0530

    Configure CORS with specific origin and credentials

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,12 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors(
+  {
+        origin: "http://34.204.192.27:3000",
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
